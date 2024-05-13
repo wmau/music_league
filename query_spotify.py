e77b41e9dbdf79207d66332d4dcbed37da684dcd
William Mau <william.mau92@gmail.com>
2024-05-13 19:27:35 -0400

    initial commit

---
+++ query_spotify.py
@@ -0,0 +1,19 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+
+class Spotify:
+    def __init__(self):
+        self.spotify = spotipy.Spotify(
+            client_credentials_manager=SpotifyClientCredentials(),
+            requests_timeout=60,
+            retries=10,
+            backoff_factor=10,
+        )
+
+    def find_artist(self, artist_name):
+        result = self.spotify.search(artist_name, type="artist", limit=1)["artists"][
+            "items"
+        ][0]
+
+        return result
